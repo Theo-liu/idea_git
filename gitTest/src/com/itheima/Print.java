451e177e82740c835349e51267e6a23170d12db8
theo <theo@itcast.cn>
2020-06-16 21:18:25 +0800

    第二次提交

--- gitTest/src/com/itheima/Print.java
+++ gitTest/src/com/itheima/Print.java
@@ -3,5 +3,6 @@ package com.itheima;
 public class Print {
     public static void main(String[] args) {
         System.out.println("this is version1");
+        System.out.println("this is version2");
     }
 }
